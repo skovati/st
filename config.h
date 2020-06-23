87ddd608b45fcc5307f1b8b7f199d18f0845318b
skovati <mail@skovati.com>
2020-06-22 21:36:26 -0500

    Add scrollback support

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TamzenForPowerline:pixelsize=18:antialias=false:autohint=true";
+static char *font = "TamzenForPowerline:pixelsize=20:antialias=false:autohint=true";
 static int borderpx = 24;
 
 /*
@@ -245,6 +245,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
